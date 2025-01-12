29d243fb7f4eb43c7ba0bc29190652ad527d6ebc
hannah68 <reihaneh.naderi1989@gmail.com>
2025-01-12 17:05:26 +0000

    initial commit

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://zlrlnwtobbvfplhcfxvw.supabase.co";
+const supabaseKey =
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpscmxud3RvYmJ2ZnBsaGNmeHZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYyNzk3MDYsImV4cCI6MjA1MTg1NTcwNn0.WXYp0Wzq69XG7U8hPCGCujEOoTaXRO7lf03cCjesrcM";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
