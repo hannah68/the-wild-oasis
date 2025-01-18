4cee0ecab634baf215fb9231d670c9f08664dbd9
hannah68 <reihaneh.naderi1989@gmail.com>
2025-01-18 18:02:19 +0000

    create cabin form

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -1,176 +1,131 @@
-import { useForm } from 'react-hook-form';
+import styled from "styled-components";
 
-import { useCreateCabin } from 'features/cabins/useCreateCabin';
-import FormRow from 'ui/FormRow';
-import Input from 'ui/Input';
-import Form from 'ui/Form';
-import Button from 'ui/Button';
-import FileInput from 'ui/FileInput';
-import { useEditCabin } from './useEditCabin';
-import { Textarea } from 'ui/Textarea';
+import Input from "../../ui/Input";
+import Form from "../../ui/Form";
+import Button from "../../ui/Button";
+import FileInput from "../../ui/FileInput";
+import { Textarea } from "../../ui/Textarea";
+import { useForm } from "react-hook-form";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
+import FormRow from "../../ui/FormRow";
 
-// We use react-hook-form to make working with complex and REAL-WORLD forms a lot easier. It handles stuff like user validation and errors. manages the form state for us, etc
-// Validating the user’s data passed through the form is a crucial responsibility for a developer.
-// React Hook Form takes a slightly different approach than other form libraries in the React ecosystem by adopting the use of uncontrolled inputs using ref instead of depending on the state to control the inputs. This approach makes the forms more performant and reduces the number of re-renders.
+const Label = styled.label`
+	font-weight: 500;
+`;
 
-// Receives closeModal directly from Modal
-function CreateCabinForm({ cabinToEdit, closeModal }) {
-  const { mutate: createCabin, isLoading: isCreating } = useCreateCabin();
-  const { mutate: editCabin, isLoading: isEditing } = useEditCabin();
-  const isWorking = isCreating || isEditing;
+const Error = styled.span`
+	font-size: 1.4rem;
+	color: var(--color-red-700);
+`;
 
-  // For an editing session
-  const { id: editId, ...editValues } = cabinToEdit || {};
-  delete editValues.created_at;
-  const isEditSession = Boolean(editId);
+function CreateCabinForm() {
+	const { register, handleSubmit, reset, getValues, formState } = useForm();
+	const queryClient = useQueryClient();
+	const { mutate, isLoading: isCreating } = useMutation({
+		mutationFn: createCabin,
+		onSuccess: () => {
+			toast.success("New cabin successfully created.");
+			queryClient.invalidateQueries({ queryKey: ["cabins"] });
+			reset();
+		},
+		onError: (err) => toast.error(err.message),
+	});
 
-  // One of the key concepts in React Hook Form is to register your component into the hook. This will make its value available for both the form validation and submission.
-  const { register, handleSubmit, formState, reset, getValues } = useForm({
-    defaultValues: isEditSession ? editValues : {},
-  });
-  const { errors } = formState;
+	const onSubmit = (data) => {
+		mutate({ ...data, image: data.image[0] });
+	};
 
-  // Invoked in ALL validation passes. Here we get access to the form data
-  const onSubmit = function (data) {
-    // No need to validate here, because it's already been done. This is REALLY nice!
+	const onError = () => {
+		//console.log(error)
+	};
 
-    const options = {
-      onSuccess: (data) => {
-        // If this component is used OUTSIDE the Modal Context, this will return undefined, so we need to test for this
-        closeModal?.();
-        reset();
-      },
-    };
+	return (
+		<Form onSubmit={handleSubmit(onSubmit, onError)}>
+			<FormRow label={"Cabin name"} error={formState.errors?.name?.message}>
+				<Input
+					type="text"
+					id="name"
+					{...register("name", { required: "This field is required" })}
+				/>
+			</FormRow>
 
-    const image = typeof data.image === 'object' ? data.image[0] : data.image;
+			<FormRow
+				label="Maximum capacity"
+				error={formState.errors?.maxCapacity?.message}
+			>
+				<Input
+					type="number"
+					id="maxCapacity"
+					disabled={isCreating}
+					{...register("maxCapacity", {
+						required: "This field is required",
+						min: { value: 1, message: "Capacity should be at least 1" },
+					})}
+				/>
+			</FormRow>
 
-    if (isEditSession)
-      editCabin(
-        {
-          newCabinData: { ...data, image },
-          id: editId,
-        },
-        options
-      );
-    else createCabin({ ...data, image }, options);
-  };
+			<FormRow
+				label="Regular price"
+				error={formState.errors?.regularPrice?.message}
+			>
+				<Input
+					type="number"
+					id="regularPrice"
+					disabled={isCreating}
+					{...register("regularPrice", {
+						required: "This field is required",
+						min: { value: 1, message: "Price should be at least 1" },
+					})}
+				/>
+			</FormRow>
 
-  // Invoked when validation fails
-  const onError = function (errors) {
-    console.log('Failed validation!', errors);
-  };
+			<FormRow label="Discount" error={formState.errors?.discount?.message}>
+				<Input
+					type="number"
+					id="discount"
+					disabled={isCreating}
+					defaultValue={0}
+					{...register("discount", {
+						required: "This field is required",
+						validate: (value) =>
+							value <= getValues().regularPrice ||
+							"Discount should be less than regular price",
+					})}
+				/>
+			</FormRow>
 
-  // By default, validation happens the moment we submit the form, so when we call handleSubmit. From them on, validation happens on the onChange event [demonstrate]. We cah change that by passing options into useForm ('mode' and 'reValidateMode')
-  // https://react-hook-form.com/api/useform
+			<FormRow
+				label="Description for website"
+				error={formState.errors?.description?.message}
+			>
+				<Textarea
+					type="number"
+					id="description"
+					disabled={isCreating}
+					defaultValue=""
+					{...register("description", { required: "This field is required" })}
+				/>
+			</FormRow>
 
-  // The registered names need to be the same as in the Supabase table. This makes it easier to send the request
-  // "handleSubmit" will validate your inputs before invoking "onSubmit"
+			<FormRow label="Cabin photo">
+				<FileInput
+					id="image"
+					accept="image/*"
+					{...register("image", { required: "This field is required" })}
+				/>
+			</FormRow>
 
-  return (
-    <Form onSubmit={handleSubmit(onSubmit, onError)} type='modal'>
-      <FormRow label='Cabin name' error={errors?.name?.message}>
-        {/* register your input into the hook by invoking the "register" function */}
-        {/* why the ...? Because this will return an object { onChange, onBlur, customer, ref }, and by spreading we then add all these to the element [show dev tools] */}
-        {/* include validation with required or other standard HTML validation rules: required, min, max, minLength, maxLength, pattern, validate */}
-        {/* errors will return when field validation fails  */}
-        <Input
-          type='text'
-          id='name'
-          disabled={isWorking}
-          {...register('name', { required: 'This field is required' })}
-        />
-      </FormRow>
-
-      <FormRow label='Maximum capacity' error={errors?.maxCapacity?.message}>
-        <Input
-          type='number'
-          id='maxCapacity'
-          disabled={isWorking}
-          {...register('maxCapacity', {
-            required: 'This field is required',
-            min: {
-              value: 1,
-              message: 'Capacity should be at least 1',
-            },
-          })}
-        />
-      </FormRow>
-
-      <FormRow label='Regular price' error={errors?.regularPrice?.message}>
-        <Input
-          type='number'
-          id='regularPrice'
-          disabled={isWorking}
-          {...register('regularPrice', {
-            required: 'This field is required',
-            min: {
-              value: 1,
-              message: 'Price should be at least 1',
-            },
-          })}
-        />
-      </FormRow>
-
-      <FormRow label='Discount' error={errors?.discount?.message}>
-        <Input
-          type='number'
-          id='discount'
-          defaultValue={0}
-          disabled={isWorking}
-          {...register('discount', {
-            required: "Can't be empty, make it at least 0",
-            validate: (value) =>
-              getValues().regularPrice >= value ||
-              'Discount should be less than regular price',
-          })}
-        />
-      </FormRow>
-
-      <FormRow
-        label='Description for website'
-        error={errors?.description?.message}
-      >
-        <Textarea
-          type='number'
-          id='description'
-          defaultValue=''
-          disabled={isWorking}
-          {...register('description', { required: 'This field is required' })}
-        />
-      </FormRow>
-
-      <FormRow label='Cabin photo' error={errors?.image?.message}>
-        <FileInput
-          id='image'
-          accept='image/*'
-          disabled={isWorking}
-          {...register('image', {
-            // required: 'This field is required',
-            required: isEditSession ? false : 'This field is required',
-
-            // VIDEO this doesn't work, so never mind about this, it's too much
-            // validate: (value) =>
-            //   value[0]?.type.startsWith('image/') || 'Needs to be an image',
-          })}
-        />
-      </FormRow>
-
-      <FormRow>
-        {/* type is an HTML attribute! */}
-        <Button
-          variation='secondary'
-          type='reset'
-          disabled={isWorking}
-          onClick={() => closeModal?.()}
-        >
-          Cancel
-        </Button>
-        <Button disabled={isWorking}>
-          {isEditSession ? 'Edit cabin' : 'Create new cabin'}
-        </Button>
-      </FormRow>
-    </Form>
-  );
+			<FormRow>
+				{/* type is an HTML attribute! */}
+				<Button variation="secondary" type="reset">
+					Cancel
+				</Button>
+				<Button disabled={isCreating}>Add cabin</Button>
+			</FormRow>
+		</Form>
+	);
 }
 
 export default CreateCabinForm;
